8ea54b25208309b20246aac8c42378b53f20376d
Minecraft365871 <minecraft_365871@outlook.com>
2026-05-30 15:43:25 +0800

    更改文件头喵(づ￣ 3￣)づ

--- bili-msg-bg.meta.js
+++ bili-msg-bg.meta.js
@@ -4,7 +4,10 @@
 // @version      2.2
 // @description  为Bilibili消息中心设置背景，兼容 BewlyBewly
 // @author       Minecraft_365871
+// @copyright    2026   Minecraft_365871 (https://github.com/Minecraft365871)
+// @license      GPL-3.0
 // @match        https://message.bilibili.com/*
+// @icon         https://www.bilibili.com/favicon.ico
 // @updateURL    https://raw.githubusercontent.com/Minecraft365871/bili-msg-bg/main/bili-msg-bg.meta.js
 // @downloadURL  https://raw.githubusercontent.com/Minecraft365871/bili-msg-bg/main/bili-msg-bg.user.js
 // @grant        none
